1d78a87e4d6e4ba51c5dffa2d5add6444def64d1
Daniela Garcia <mdgarciaaguilar@hotmail.com>
2020-09-21 18:01:35 -0500

    react router funcional

---
+++ client/src/components/pages/Login.js
@@ -0,0 +1,18 @@
+import React, {useEffect, useState, useContext} from 'react';
+import {Link} from 'react-router-dom';
+
+const Login = () => {
+    return ( 
+      <>
+        <h1 >Login</h1>
+        <Link to="/signup" >
+            Sign up
+        </Link>
+
+        
+        
+      </>
+    );
+}
+ 
+export default Login;
---
+++ client/src/components/pages/Signup.js
@@ -0,0 +1,17 @@
+import React, {useEffect, useState, useContext} from 'react';
+import {Link} from 'react-router-dom';
+
+const Signup = () => {
+    return ( 
+      <>
+        <h1 >Signup</h1>
+
+        {/* <Link to="/signup">
+            Sign up
+        </Link> */}
+        
+      </>
+    );
+}
+ 
+export default Signup;
