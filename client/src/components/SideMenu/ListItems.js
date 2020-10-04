a1b4c8370c64c1c1302a0ef85f0a2f799124cae0
Antonio Aleman <antonio.9714@gmail.com>
2020-10-03 23:15:20 -0500

    side menu funcional como component

---
+++ client/src/components/SideMenu/ListItems.js
@@ -0,0 +1,77 @@
+import React from "react";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import PeopleIcon from "@material-ui/icons/Person";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import AccountBalanceWalletIcon from "@material-ui/icons/AccountBalanceWallet";
+import { useNavigate } from "react-router-dom";
+
+const ListItems = () => {
+   const navigate = useNavigate();
+   return (
+      <div>
+      <ListItem
+         button
+         onClick={() => {
+            navigate("/inicio");
+         }}
+      >
+         <ListItemIcon>
+            <div className="drawer-icon">
+               <DashboardIcon />
+            </div>
+         </ListItemIcon>
+         <div className="texto-primary">
+          <ListItemText primary="Inicio" />
+         </div>
+      </ListItem>
+      
+      <ListItem
+         button
+         onClick={() => {
+            navigate("/mi-perfil");
+         }}
+      >
+         <ListItemIcon>
+            <div className="drawer-icon">
+               <PeopleIcon />
+            </div>
+         </ListItemIcon>
+         <div className="texto-primary">
+          <ListItemText primary="Mi perfil" />
+         </div>
+      </ListItem>
+      
+
+      <ListItem
+         button
+         onClick={() => {
+            navigate("/mis-oportunidades");
+         }}
+      >
+         <ListItemIcon>
+            <div className="drawer-icon">
+               <AccountBalanceWalletIcon />
+            </div>
+         </ListItemIcon>
+         <div className="texto-primary">
+          <ListItemText primary="Mis oportunidades" />
+         </div>
+      </ListItem>
+
+      <ListItem button>
+         <ListItemIcon>
+            <div className="drawer-icon">
+               <ExitToAppIcon />
+            </div>
+         </ListItemIcon>
+         <div className="texto-primary">
+          <ListItemText primary="Cerrar sesión" />
+         </div>
+      </ListItem>
+   </div>
+   );
+}
+ export default ListItems;
