065cdc6892f94e4f102577d201883360ec0cb726
Daniela Garcia <mdgarciaaguilar@hotmail.com>
2020-10-17 20:18:26 -0500

    dashboard de socio, cliente y admin, se agrego spinner mientras se hace la llamada a la base de datos y se muestra mensaje si no hay rfps

--- client/src/components/pages/Inicio.js
+++ client/src/components/pages/Inicio.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Card from '@material-ui/core/Card';
 import axios from 'axios';
 
 import "../../styles/globalStyles.css";
@@ -19,6 +21,9 @@ const Inicio = () => {
    // state de lista de RFPs
    const [listaRfps, guardarListaRfps] = useState([]);
 
+   // state de control de si ya se hizo la llamada a la base de datos
+   const [llamada, guardarLlamada] = useState('false');
+
    // Obtener tipo de usuario
    const userType = sessionStorage.getItem("userType");
 
@@ -29,33 +34,39 @@ const Inicio = () => {
             .then((res) => {
                // guardar lista de RFPs en state
                guardarListaRfps(res.data);
+               // actualizar variable de control
+               guardarLlamada('true');
             })
             .catch((error) => {
                console.log(error);
             })
-      }
+      };
       const obtenerListaRfpsCliente = () => {
          axios
              .get("/RFP/get-rfp-cliente", config)
              .then((res) => {
                 // guardar lista de RFPs en state
                 guardarListaRfps(res.data);
+                // actualizar variable de control
+               guardarLlamada('true');
              })
              .catch((error) => {
                 console.log(error);
              })
-       }
+       };
        const obtenerListaRfpsSocio = () => {
          axios
              .get("/RFP/get-rfp-socio", config)
              .then((res) => {
                 // guardar lista de RFPs en state
                 guardarListaRfps(res.data);
+                // actualizar variable de control
+               guardarLlamada('true');
              })
              .catch((error) => {
                 console.log(error);
              })
-       }
+       };
       if (userType === 'admin') obtenerListaRfpsAdmin();
       if (userType === 'cliente') obtenerListaRfpsCliente();
       if (userType === 'socio') obtenerListaRfpsSocio();
@@ -74,8 +85,12 @@ const Inicio = () => {
             direction="row"
             className="container-dashboard "
          >
-            {listaRfps.length === 0 ? console.log('Vacia') : console.log('Existe')}
-            {console.log(listaRfps)}
+            {llamada === 'false' ? (<CircularProgress color="secondary"/>) : null}
+            {llamada === 'true' 
+               ? (listaRfps.length === 0 
+                  ? (<Card className="cardMensaje"><Typography>No hay RFPs para mostrar</Typography></Card>)
+                  : console.log('Existe'))
+               : null}
             {userType === 'socio' 
                ? (listaRfps.map(rfp => (<RfpCardSocio key={rfp._id} rfp={rfp} />)))
                : (listaRfps.map(rfp => (<RfpCardCliente key={rfp._id} rfp={rfp} />)))
