ca5e4941088523ec4583e6db73afeea5ffeb2e80
Daniela Garcia <mdgarciaaguilar@hotmail.com>
2020-11-12 20:38:21 -0600

    frontend de calendario

--- client/src/components/pages/Calendario.js
+++ client/src/components/pages/Calendario.js
@@ -3,14 +3,22 @@ import { Grid, Typography } from "@material-ui/core";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Card from '@material-ui/core/Card';
 import axios from 'axios';
-// import Calendar from "../Calendar.js";
+
+import { Calendar, momentLocalizer, Views } from 'react-big-calendar'
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import moment from 'moment'
 
 
 import "../../styles/globalStyles.css";
 import SideMenu from "../SideMenu/SideMenu";
-import RfpCardCliente from "../Cards/RfpCardCliente";
-import RfpCardSocio from "../Cards/RfpCardSocio";
 import FabButton from "../ui/FabButton";
+
+const ColoredDateCellWrapper = ({ children }) =>
+  React.cloneElement(React.Children.only(children), {
+    style: {
+      backgroundColor: '#EE5D36',
+    },
+  })
 
 const Calendario = () => {
    // state de lista de RFPs
@@ -23,6 +31,26 @@ const Calendario = () => {
 
    // Obtener tipo de usuario
    const userType = sessionStorage.getItem("userType");
+
+   // Setup the localizer by providing the moment (or globalize) Object
+   // to the correct localizer.
+   //const localizer = BigCalendar.momentLocalizer(moment) // or globalizeLocalizer
+   const [myEventsList, guardarmyEventsList] = useState([]);
+   var events = [
+      {
+         id: 2,
+         title: 'Evento 1',
+         start: new Date(2020, 10, 15, 0, 0, 0),
+         end: new Date(2020, 10, 15, 1, 0, 0),
+       },
+       {
+         id: 3,
+         title: 'Evento 2',
+         start: new Date(2020, 10, 25, 0, 0, 0),
+         end: new Date(2020, 10, 25, 1, 0, 0),
+       },
+   ];
+   const localizer = momentLocalizer(moment)
 
    useEffect(() => {
       const config = {
@@ -96,6 +124,10 @@ const Calendario = () => {
            }
        }
     }
+
+    let allViews = Object.keys(Views).map(k => Views[k])
+    console.log(events);
+
       return (
       <>
          <SideMenu />
@@ -106,21 +138,28 @@ const Calendario = () => {
 
          <Grid
             container
-            direction="row"
-            className="container-dashboard "
+            direction="column"
+            className="container-calendar-white "
          >
-            {/* <Calendar /> */}
-            {/* {llamada === 'false' ? (<CircularProgress color="secondary"/>) : null}
-            {llamada === 'true'
-               ? (listaRfps.length === 0
-                  ? (<Card className="cardMensaje"><Typography>No hay RFPs para mostrar</Typography></Card>)
-                  : console.log('Existe'))
-               : null}
-            {userType === 'socio'
-               ? (listaRfps.map(rfp => (<RfpCardSocio key={rfp._id} rfp={rfp} />)))
-               : (listaRfps.map(rfp => (<RfpCardCliente key={rfp._id} rfp={rfp} />)))
-            } */}
-
+            {/* <BigCalendar
+               localizer={localizer}
+               events={myEventsList}
+               startAccessor="start"
+               endAccessor="end"
+            /> */}
+            <div className="container-calendar">
+               <Calendar 
+                  localizer={localizer} 
+                  events={events}
+                  views={allViews}
+                  step={60}
+                  components={{
+                     timeSlotWrapper: ColoredDateCellWrapper,
+                   }}
+               />
+            </div>
+            
+           
          </Grid>
       </>
    );
