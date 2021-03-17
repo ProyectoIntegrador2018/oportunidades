e2e02dc19ba3ab7db5fe7f518bbb2fbe11ae581d
Ariel Mendez <arielmendez735@gmail.com>
2021-03-17 14:35:37 -0600

    Refactor notification service to use bull queue

--- server/services/NotificationService.js
+++ server/services/NotificationService.js
@@ -1,19 +1,19 @@
 const UserModel = require("../models/User");
-const notificationTypes = require("../utils/NotificationTypes");
+const { NUEVA_OPORTUNIDAD } = require("../utils/NotificationTypes");
 const detallesNotifController = require("../controllers/DetallesNotificacionController");
 const notificacionController = require("../controllers/NotificacionController");
 const usuarioNotificacion = require("../controllers/UsuarioNotificacionController");
 
 const notificationService = {};
 
-notificationService.notificacionNuevaOportunidad = (rfp) => {
+notificationService.notificacionNuevaOportunidad = (job) => {
   return new Promise((resolve, reject) => {
-    const detalles = { rfp: rfp._id };
+    const detalles = { rfp: job.rfpId };
     detallesNotifController
       .createDetalles(detalles)
       .then((detallesNotif) => {
         const rawNotificacion = {
-          tipo: notificationTypes.NUEVA_OPORTUNIDAD,
+          tipo: NUEVA_OPORTUNIDAD,
           date: new Date(),
           detalles: detallesNotif._id,
         };
@@ -49,7 +49,7 @@ notificationService.notificacionNuevaOportunidad = (rfp) => {
                 reject(error);
               });
           });
-          resolve(rfp);
+          resolve();
         });
       })
       .catch((error) => {
