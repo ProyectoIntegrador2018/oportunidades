d73895fffc6bbfa5f86f0076093caba3864102ab
Ariel Mendez <arielmendez735@gmail.com>
2021-04-12 22:03:09 -0500

    Add cron function to update last day rfp status

--- server/services/EventScheduler.js
+++ server/services/EventScheduler.js
@@ -1,12 +1,42 @@
+const RFPModel = require("../models/RFP");
 const EventModel = require("../models/Event");
 const cron = require("node-cron");
 const eventScheduler = {};
 
+const nextStatus = {
+  Activo: "Sesión 1 llevada a cabo",
+  "Sesión 1 llevada a cabo": "Sesión 2 llevada a cabo",
+  "Esperando respuesta": "Cerrada",
+  Cerrada: "Cerrada",
+};
+
+const getNextStatus = (status) => nextStatus[status];
+
 eventScheduler.checkForEventStatusUpdate = () => {
-  cron.schedule("* * * * *", () => {
+  cron.schedule("0 1 * * *", () => {
     EventModel.getEventsFromLastDay()
       .then((events) => {
-        console.log(events);
+        events.map((event) => {
+          const rfpId = event.rfp;
+          RFPModel.findById(rfpId)
+            .then((rfp) => {
+              const nextStatus = getNextStatus(rfp.estatus);
+              RFPModel.update(
+                { _id: rfpId },
+                {
+                  estatus: nextStatus,
+                },
+                function (err, result) {
+                  if (err) {
+                    console.log(err);
+                  } else {
+                    console.log(result);
+                  }
+                }
+              );
+            })
+            .catch((error) => console.log(error));
+        });
       })
       .catch((error) => console.log(error));
   });
