b91b72f9d6333aa926c105007af0b24e8f86f2e4
Raul Herrera <raul_ehs@hotmail.com>
2021-04-22 14:20:22 -0500

    Generate & attach pdf to email only for NUEVA_OPORTUNIDAD emails

--- server/services/MailService.js
+++ server/services/MailService.js
@@ -29,13 +29,15 @@ mailService.sendEmail = (jobData) => {
       subject: mailContent.subject,
       text: `Hola ${destinatario.name}, ${mailContent.text}`,
       html: `<p>Hola ${destinatario.name}, ${mailContent.html}`,
-      attachments: {
+    };
+
+    if (mailContent.attachments) {
+      mailOptions.attachments = {
         filename: mailContent.attachments.filename,
         content: mailContent.attachments.content,
         encoding: 'base64',
       }
-    };
-
+    }
 
     transporter.sendMail(mailOptions, (err, info) => {
       if (err) {
@@ -74,75 +76,72 @@ mailService.buildMailContent = (tipoNotificacion, rfp) => {
         Nombre: ${rfp.nombrecliente}
         Posición: ${rfp.posicioncliente}
 
-
-
         Gracias,
         Notificaciones de CSOFTMTY`;
 
         mailOptions.html = `
         <!doctype html>
-          <html>
-            <head>
-              <style>
-              h1{
-                margin-top:40px;
-                text-align:center;
-                font-family:Helvetica;
-              }
-              h3{
-                font-weight:bold;
-                font-family:Helvetica;
-              }
-              p{
-                font-family:Helvetica;
-              }
-              b{
-                font-family:Helvetica;
-              }
-              img{
-                display:block;
-                margin-left:auto;
-                margin-right:auto;
-                width:50%;
-              }
-              body{
-                margin:50px;
-              }
-              </style>
-            </head>
-              <h1>Nueva Oportunidad Comercial</h1>
-                <body>
-                  <div>
-                    <h3>Datos generales</h3>
-                    <p><b>Nombre de la Oportunidad Comercial:</b> ${rfp.nombreOportunidad}<br>
-                    <b>Objetivo de la oportunidad:</b> ${rfp.objetivoOportunidad}<br>
-                    <b>Descripción funcional de la oportunidad:</b> ${rfp.descripcionFuncional}<br>
-                  </div>
-                    <div>
-                      <h3>Detalle de la oportunidad</h3>
-                      <b>Requerimientos obligatorios:</b> ${rfp.requerimientosObligatorios}<br>
-                      <b>Fechas relevantes:</b> ${rfp.fechasRelevantes}<br><br>
-                    </div>
-                      <div>
-                        <h3>Estatus de la necesidad</h3>
-                        <b>¿Ha sido aprobada por el área usuaria?:</b> ${rfp.aprobadaAreaUsuario}<br>
-                        <b>¿Ha sido aprobada por el área de TI?:</b> ${rfp.aprobadaAreaTI}<br>
-                        <b>¿Tiene un presupuesto asignado?:</b> ${rfp.presupuestoAsignado}<br>
-                        <b>Tipo general del proyecto:</b> ${rfp.tipoGeneralProyecto}<br>
-                        <b>Tipo específico del proyecto:</b> ${rfp.tipoEspecificoProyecto}<br>
-                        <b>Comentarios adicionales:</b> ${rfp.comentariosAdicionales}</p>
-                      </div>
-                        <div>
-                          <h3>Datos de contacto</h3>
-                          <p><b>Nombre:</b> ${rfp.nombrecliente}<br>
-                          <b>Posición:</b> ${rfp.posicioncliente}<br>
-                        </div>
-                          <br>
-                 </body>
-          </html>`;
+        <html>
+          <head>
+            <style>
+            h1{
+              margin-top:40px;
+              text-align:center;
+              font-family:Helvetica;
+            }
+            h3{
+              font-weight:bold;
+              font-family:Helvetica;
+            }
+            p{
+              font-family:Helvetica;
+            }
+            b{
+              font-family:Helvetica;
+            }
+            img{
+              display:block;
+              margin-left:auto;
+              margin-right:auto;
+              width:50%;
+            }
+            body{
+              margin:50px;
+            }
+            </style>
+          </head>
+            <h1>Nueva Oportunidad Comercial</h1>
+            <body>
+              <div>
+                <h3>Datos generales</h3>
+                <p><b>Nombre de la Oportunidad Comercial:</b> ${rfp.nombreOportunidad}<br>
+                <b>Objetivo de la oportunidad:</b> ${rfp.objetivoOportunidad}<br>
+                <b>Descripción funcional de la oportunidad:</b> ${rfp.descripcionFuncional}<br>
+              </div>
+              <div>
+                <h3>Detalle de la oportunidad</h3>
+                <b>Requerimientos obligatorios:</b> ${rfp.requerimientosObligatorios}<br>
+                <b>Fechas relevantes:</b> ${rfp.fechasRelevantes}<br><br>
+              </div>
+              <div>
+                <h3>Estatus de la necesidad</h3>
+                <b>¿Ha sido aprobada por el área usuaria?:</b> ${rfp.aprobadaAreaUsuario}<br>
+                <b>¿Ha sido aprobada por el área de TI?:</b> ${rfp.aprobadaAreaTI}<br>
+                <b>¿Tiene un presupuesto asignado?:</b> ${rfp.presupuestoAsignado}<br>
+                <b>Tipo general del proyecto:</b> ${rfp.tipoGeneralProyecto}<br>
+                <b>Tipo específico del proyecto:</b> ${rfp.tipoEspecificoProyecto}<br>
+                <b>Comentarios adicionales:</b> ${rfp.comentariosAdicionales}</p>
+              </div>
+              <div>
+                <h3>Datos de contacto</h3>
+                <p><b>Nombre:</b> ${rfp.nombrecliente}<br>
+                <b>Posición:</b> ${rfp.posicioncliente}<br>
+              </div>
+              <br>
+            </body>
+        </html>`;
         break;
 
-      
       case NUEVA_PARTICIPACION:
         mailOptions.subject = "Un socio de CSOFTMTY ha aplicado a tu Oportunidad Comercial";
         mailOptions.text = `queremos informarte que el socio ${rfp.participanteName} ha aplicado a tu Oportunidad Comercial "${rfp.nombreOportunidad}".`
@@ -169,18 +168,23 @@ mailService.buildMailContent = (tipoNotificacion, rfp) => {
         reject("Invalid notificationType");
     }
 
-    mailOptions.text += "\n\nGracias,\nNotificaciones de CSOFTMTY";
-    mailOptions.html += `<p>Gracias,<br>
-      Notificaciones de CSOFTMTY</p>
-      <img src="https://www.csoftmty.org/assets/images/header/logo.png" alt="logo_csoftmty"/>`
-    
-    generatePdf(mailOptions.html).then((base64String) => {
-      mailOptions.attachments = {
-        filename: `${rfp.nombreOportunidad}.pdf`,
-        content: base64String,
-      };
+    if (tipoNotificacion === NUEVA_OPORTUNIDAD) {
+      generatePdf(mailOptions.html).then((base64String) => {
+        mailOptions.attachments = {
+          filename: `${rfp.nombreOportunidad}.pdf`,
+          content: base64String,
+        };
+        resolve(mailOptions);
+      });
+    }
+    else {
+      mailOptions.text += "\n\nGracias,\nNotificaciones de CSOFTMTY";
+      mailOptions.html += `<p>Gracias,<br>
+        Notificaciones de CSOFTMTY</p>
+        <img src="https://www.csoftmty.org/assets/images/header/logo.png" alt="logo_csoftmty"/>`
       resolve(mailOptions);
-    });
+    }
+    
   });
 };
 
@@ -192,4 +196,5 @@ const generatePdf = (htmlToPdf) => {
     });
   });
 };
+
 module.exports = mailService;
