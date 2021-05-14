d8716027ebe73932d2e723f2eb87ca42442b342e
Ariel Mendez <arielmendez735@gmail.com>
2021-05-14 17:37:50 -0500

    update mail enabled env var condition

--- server/services/NotificationService.js
+++ server/services/NotificationService.js
@@ -24,7 +24,7 @@ const mailQueue = require("./MailQueue");
 
 const notificationService = {};
 const SUCCESS_RESP = { success: 1 };
-const MAIL_ENABLED = process.env.MAIL_ENABLED;
+const MAIL_ENABLED = process.env.MAIL_ENABLED === 'true' ? true : false;
 
 notificationService.notificacionNuevaOportunidad = (job) => {
   return new Promise((resolve, reject) => {
