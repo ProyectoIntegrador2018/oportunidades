c3e5af3e5cd94bee764a1a7417984a79c05488f1
Raul Herrera <raul_ehs@hotmail.com>
2021-05-17 16:26:00 -0500

    Formatting

--- client/src/components/Dialogs/NuevoEvento.js
+++ client/src/components/Dialogs/NuevoEvento.js
@@ -10,112 +10,114 @@ import CloseIcon from "@material-ui/icons/Close";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import DatePicker from "react-datepicker";
-import {FormLabel} from '@material-ui/core';
+import { FormLabel } from "@material-ui/core";
 import "react-datepicker/dist/react-datepicker.css";
-import { addDays } from 'date-fns';
+import { addDays } from "date-fns";
 import Axios from "axios";
-import moment from 'moment';
-import { registerLocale, setDefaultLocale } from  "react-datepicker";
-import es from 'date-fns/locale/es';
-registerLocale('es', es);
+import moment from "moment";
+import { registerLocale, setDefaultLocale } from "react-datepicker";
+import es from "date-fns/locale/es";
+registerLocale("es", es);
 
 const styles = (theme) => ({
-   root: {
-      margin: 0,
-      padding: theme.spacing(2),
-   },
-   closeButton: {
-      position: "absolute",
-      right: theme.spacing(1),
-      top: theme.spacing(1),
-      color: theme.palette.grey[500],
-   },
-   textField: {
-      marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(1),
-      width: 200,
-   },
-   fechaDiv: {
-      height: 200,
-      margin: theme.spacing(2),
-   },
-   title: {
-      
-   },
-   marginBott: {
-      marginBottom: theme.spacing(1),
-   }
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: "absolute",
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+  fechaDiv: {
+    height: 200,
+    margin: theme.spacing(2),
+  },
+  title: {},
+  marginBott: {
+    marginBottom: theme.spacing(1),
+  },
 });
 
 const DialogTitle = withStyles(styles)((props) => {
-   const { children, classes, onClose, ...other } = props;
-   return (
-      <MuiDialogTitle disableTypography className={classes.root} {...other}>
-         <Typography variant="h6">{children}</Typography>
-         {onClose ? (
-            <IconButton
-               aria-label="close"
-               className={classes.closeButton}
-               onClick={onClose}
-            >
-               <CloseIcon />
-            </IconButton>
-         ) : null}
-      </MuiDialogTitle>
-   );
+  const { children, classes, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          className={classes.closeButton}
+          onClick={onClose}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
 });
 
 const DialogContent = withStyles((theme) => ({
-   root: {
-      padding: theme.spacing(2),
-   },
+  root: {
+    padding: theme.spacing(2),
+  },
 }))(MuiDialogContent);
 
 const DialogActions = withStyles((theme) => ({
-   root: {
-      margin: 0,
-      padding: theme.spacing(1),
-   },
+  root: {
+    margin: 0,
+    padding: theme.spacing(1),
+  },
 }))(MuiDialogActions);
 
- // Función para date picker
- let handleColor = time => {
-   return time.getHours() > 12 ? "text-success" : "text-error";
- };
+// Función para date picker
+let handleColor = (time) => {
+  return time.getHours() > 12 ? "text-success" : "text-error";
+};
 
 // Función que redondea una Date hacia arriba, al intervalo de 30 minutos más cercano
 const roundUpTime = (date) => {
   if (date.getMinutes() >= 30) {
     date.setHours(date.getHours() + 1, 0, 0, 0);
-  }
-  else {
+  } else {
     date.setHours(date.getHours(), 30, 0, 0, 0);
   }
   return date;
 };
 
 export default function NuevoEvento(params) {
-   const [open, setOpen] = React.useState(false);
-   const [nombre, guardarNombre] = React.useState('');
-  const [horaActualRedondeada, guardarHoraActualRedondeada] = React.useState( roundUpTime(new Date()) );
-   const [fecha, guardarFecha] = React.useState(horaActualRedondeada);
-   const [link, guardarLink] = React.useState('');
+  const [open, setOpen] = React.useState(false);
+  const [nombre, guardarNombre] = React.useState("");
+  const [horaActualRedondeada, guardarHoraActualRedondeada] = React.useState(
+    roundUpTime(new Date())
+  );
+  const [fecha, guardarFecha] = React.useState(horaActualRedondeada);
+  const [link, guardarLink] = React.useState("");
   const [excludedTimes, setExcludedTimes] = React.useState([]);
 
-   const config = {
-      headers: {
-         Authorization: "Bearer " + sessionStorage.getItem("token"),
-         "Content-Type": "application/json",
-      },
-   };
-   
-   useEffect(() => {
+  const config = {
+    headers: {
+      Authorization: "Bearer " + sessionStorage.getItem("token"),
+      "Content-Type": "application/json",
+    },
+  };
+
+  useEffect(() => {
     const haceUnaHora = new Date(horaActualRedondeada);
     haceUnaHora.setHours(haceUnaHora.getHours() - 1);
 
-    Axios.get("/events/get-occupied-event-times/" + haceUnaHora.toISOString(), config)
+    Axios.get(
+      "/events/get-occupied-event-times/" + haceUnaHora.toISOString(),
+      config
+    )
       .then((res) => {
-        const occupiedTimes = res.data.occupiedTimes.map(elem => {
+        const occupiedTimes = res.data.occupiedTimes.map((elem) => {
           return new Date(elem);
         });
 
@@ -125,36 +127,42 @@ export default function NuevoEvento(params) {
         console.log(error);
       });
 
-      setOpen(params.isOpen);
-   }, [params]);
+    setOpen(params.isOpen);
+  }, [params]);
 
-   const handleClose = () => {
-      params.setModalIsOpen(false);
-      setOpen(false);
-   };
-   const handleSave = () => {
-      // Guardar el nuevo usuario en la base de datos
-      Axios.post("/events/", {
-         name: nombre,
-         date: fecha,
-         link: link,
-         rfp: params.rfp
-      }, config).then(res => {
-         window.location.reload();
-      }).catch(error => {
-         console.log(error);
+  const handleClose = () => {
+    params.setModalIsOpen(false);
+    setOpen(false);
+  };
+  const handleSave = () => {
+    // Guardar el nuevo usuario en la base de datos
+    Axios.post(
+      "/events/",
+      {
+        name: nombre,
+        date: fecha,
+        link: link,
+        rfp: params.rfp,
+      },
+      config
+    )
+      .then((res) => {
+        window.location.reload();
       })
-   };
-
-  const isExcludedDate = () => {
-    return excludedTimes.includes(fecha) || fecha < new Date() ;
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
-   const isSendDisabled = () => {
+  const isExcludedDate = () => {
+    return excludedTimes.includes(fecha) || fecha < new Date();
+  };
+
+  const isSendDisabled = () => {
     return nombre.length < 1 || link.length < 1 || isExcludedDate();
   };
 
-  const excludedTimesInSelectedDay = excludedTimes.filter(date => {
+  const excludedTimesInSelectedDay = excludedTimes.filter((date) => {
     return date.getDate() === fecha.getDate();
   });
 
@@ -165,66 +173,81 @@ export default function NuevoEvento(params) {
       ? now
       : new Date(new Date().setHours(0, 0, 0, 0));
 
-   return (
-      <div>
-         <Dialog
-            onClose={handleClose}
-            aria-labelledby="customized-dialog-title"
-            open={open}
-            maxWidth='md'
-            fullWidth={true}
-            scroll="paper"
-         >
-            <DialogTitle id="customized-dialog-title" onClose={handleClose}>
-               Agregar Evento
-            </DialogTitle>
-            <DialogContent dividers>
-               <div className="mb-0">
-                  <FormLabel className="mb-1">Selecciona la fecha de la siguiente reunión</FormLabel>
-               </div>
-               <DatePicker
-                    showTimeSelect
-                    selected={fecha}
-                    onChange={date => guardarFecha(date)}
-                    excludeTimes={excludedTimesInSelectedDay}
-                    timeClassName={handleColor}
-                    timeCaption="Hora"
-                    dateFormat="dd/MM/yyyy h:mm aa"
-                    timeFormat="h:mm aa"
-                    minDate={now}
-                    minTime={dateMinTime}
-                    maxTime={new Date(new Date().setHours(23, 59, 0, 0))}
-                    //timeIntervals={15}
-                    locale="es"
-                    title="Selecciona un horario"
-               />
-               <div>{isExcludedDate() ? <p className="error-titulo-rojo">El horario seleccionado no está disponible</p> : <br/>}</div>
-               <TextField
-                  id="nombre"
-                  className={styles.textField}
-                  label="Nombre"
-                  margin="dense"
-                  fullWidth
-                  defaultValue={nombre}
-                  onChange={(event) => guardarNombre(event.target.value)}
-               />
-               <TextField
-                  id="link"
-                  className={styles.marginBott}
-                  label="Link"
-                  margin="normal"
-                  fullWidth
-                  defaultValue={link}
-                  onChange={(event) => guardarLink(event.target.value)}
-               />
-               <div className="mb-4">  </div>
-            </DialogContent>
-            <DialogActions >
-               <Button autoFocus onClick={handleSave} color="primary" disabled={isSendDisabled()}>
-                  GUARDAR EVENTO
-               </Button>
-            </DialogActions>
-         </Dialog>
-      </div>
-   );
+  return (
+    <div>
+      <Dialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+        maxWidth="md"
+        fullWidth={true}
+        scroll="paper"
+      >
+        <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+          Agregar Evento
+        </DialogTitle>
+        <DialogContent dividers>
+          <div className="mb-0">
+            <FormLabel className="mb-1">
+              Selecciona la fecha de la siguiente reunión
+            </FormLabel>
+          </div>
+          <DatePicker
+            showTimeSelect
+            selected={fecha}
+            onChange={(date) => guardarFecha(date)}
+            excludeTimes={excludedTimesInSelectedDay}
+            timeClassName={handleColor}
+            timeCaption="Hora"
+            dateFormat="dd/MM/yyyy h:mm aa"
+            timeFormat="h:mm aa"
+            minDate={now}
+            minTime={dateMinTime}
+            maxTime={new Date(new Date().setHours(23, 59, 0, 0))}
+            //timeIntervals={15}
+            locale="es"
+            title="Selecciona un horario"
+          />
+          <div>
+            {isExcludedDate() ? (
+              <p className="error-titulo-rojo">
+                El horario seleccionado no está disponible
+              </p>
+            ) : (
+              <br />
+            )}
+          </div>
+          <TextField
+            id="nombre"
+            className={styles.textField}
+            label="Nombre"
+            margin="dense"
+            fullWidth
+            defaultValue={nombre}
+            onChange={(event) => guardarNombre(event.target.value)}
+          />
+          <TextField
+            id="link"
+            className={styles.marginBott}
+            label="Link"
+            margin="normal"
+            fullWidth
+            defaultValue={link}
+            onChange={(event) => guardarLink(event.target.value)}
+          />
+          <div className="mb-4"> </div>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            autoFocus
+            onClick={handleSave}
+            color="primary"
+            disabled={isSendDisabled()}
+          >
+            GUARDAR EVENTO
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 }
--- server/routes/eventapi.js
+++ server/routes/eventapi.js
@@ -6,63 +6,68 @@ const eventController = require("../controllers/EventController");
 const router = express.Router();
 
 router.get("/get-user-events", userMiddleware, (req, res) => {
-   eventController
-      .getUserEvents(req.user._id)
-      .then((events) => {
-         return res.send({ events });
-      })
-      .catch((err) => {
-         return res.status(401).send({ err });
-      });
+  eventController
+    .getUserEvents(req.user._id)
+    .then((events) => {
+      return res.send({ events });
+    })
+    .catch((err) => {
+      return res.status(401).send({ err });
+    });
 });
 
 router.post("/", userMiddleware, (req, res) => {
-   eventController
-      .createEvent(req.body)
-      .then((newEvent) => {
-         eventController.assignToUsers(req.body.rfp, newEvent._id, newEvent, req.user._id);
-      })
-      .then((usersIds) => {
-         return res.send({
-            message: "Event created successfully",
-         });
-      })
-      .catch((err) => {
-         return res.status(401).send({ err });
+  eventController
+    .createEvent(req.body)
+    .then((newEvent) => {
+      eventController.assignToUsers(
+        req.body.rfp,
+        newEvent._id,
+        newEvent,
+        req.user._id
+      );
+    })
+    .then((usersIds) => {
+      return res.send({
+        message: "Event created successfully",
       });
+    })
+    .catch((err) => {
+      return res.status(401).send({ err });
+    });
 });
 
 router.delete("/remove-from-event/:id", userMiddleware, (req, res) => {
-   eventController
-      .deleteUserFromEvent(req.user._id, req.params.id)
-      .then((msg) => {
-         return res.send({ msg });
-      })
-      .catch((err) => {
-         return res.status(401).send({ err });
-      });
+  eventController
+    .deleteUserFromEvent(req.user._id, req.params.id)
+    .then((msg) => {
+      return res.send({ msg });
+    })
+    .catch((err) => {
+      return res.status(401).send({ err });
+    });
 });
 
 router.post("/add-to-event/:id", userMiddleware, (req, res) => {
-   eventController
-      .addUserToEvent(req.user._id, req.params.id)
-      .then((update) => {
-         return res.send(update);
-      })
-      .catch((err) => {
-         return res.status(401).send({ err });
-      });
+  eventController
+    .addUserToEvent(req.user._id, req.params.id)
+    .then((update) => {
+      return res.send(update);
+    })
+    .catch((err) => {
+      return res.status(401).send({ err });
+    });
 });
 
 router.get("/get-rfp-events/:id", userMiddleware, (req, res) => {
-   eventController
-      .getRFPEvents(req.params.id)
-      .then((events) => {
-         return res.send({ events });
-      })
-      .catch((err) => {
-         return res.status(401).send({ err });
-      });
+  eventController
+    .getRFPEvents(req.params.id)
+    .then((events) => {
+      return res.send({ events });
+    })
+    .catch((err) => {
+      return res.status(401).send({ err });
+    });
 });
 
 router.get("/get-occupied-event-times/:date", userMiddleware, (req, res) => {
@@ -77,43 +82,47 @@ router.get("/get-occupied-event-times/:date", userMiddleware, (req, res) => {
 });
 
 router.patch("/:id", userMiddleware, (req, res) => {
-   const updates = req.body;
-   const allowedUpdates = ["name", "date", "link"];
-   const isValidUpdate = Object.keys(updates).every((update) => allowedUpdates.includes(update));
-   if (!isValidUpdate) {
-      return res.status(401).send({
-         error: "invalid update fields",
-         success: 0,
-      });
-   }
-   eventController
-      .editEvent(req.params.id, updates)
-      .then((updatedEvent) => {
-         return res.send({ updatedEvent });
-      })
-      .catch((err) => {
-         return res.status(401).send({ err });
-      });
+  const updates = req.body;
+  const allowedUpdates = ["name", "date", "link"];
+  const isValidUpdate = Object.keys(updates).every((update) =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidUpdate) {
+    return res.status(401).send({
+      error: "invalid update fields",
+      success: 0,
+    });
+  }
+  eventController
+    .editEvent(req.params.id, updates)
+    .then((updatedEvent) => {
+      return res.send({ updatedEvent });
+    })
+    .catch((err) => {
+      return res.status(401).send({ err });
+    });
 });
 
 router.delete("/:id", userMiddleware, (req, res) => {
-   eventController.deleteEvent(req.params.id)
-   .then(deletedEvent => {
+  eventController
+    .deleteEvent(req.params.id)
+    .then((deletedEvent) => {
       return res.send({ deletedEvent });
-   })
-   .catch(err => {
+    })
+    .catch((err) => {
       return res.status(401).send({ err });
-   })
-})
+    });
+});
 
 router.get("/all-events", adminMiddleware, (req, res) => {
-   eventController.getAllEvents()
-   .then(events => {
+  eventController
+    .getAllEvents()
+    .then((events) => {
       return res.send({ events });
-   })
-   .catch(err => {
+    })
+    .catch((err) => {
       return res.status(401).send({ err });
-   })
-})
+    });
+});
 
 module.exports = router;
